dca3f76ba54fa4c9c4c3fe822cf98786c56d3c17
BigPanda <15038302539@163.com>
2016-12-04 12:41:05 +0800

    addlld

---
+++ day14/jQuery02/WebRoot/js/d4.js
@@ -0,0 +1,7 @@
+window.onload = function(){
+	var obj = document.getElementById('d1');
+	obj.onclick=function(){
+	this.innerHTML = 'hello java';
+	};
+};
+
---
+++ day14/jQuery02/WebRoot/js/d5.js
@@ -0,0 +1,14 @@
+$(function(){
+	$('#d1').click(function(){
+		//this.innerHTML = 'hello perl';
+		$(this).css('font-size','80px');
+	});
+});
+
+$(function(){
+	$('#d1').click(function(){
+		
+	});
+});
+
+
